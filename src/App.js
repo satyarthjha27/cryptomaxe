cdc24dec6b275049e442291fb091c3a4d58aabb2
satyarthjha <satyarth.jha@mybliss.co>
2022-02-02 11:03:18 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const App = () => {
   </Layout>
   <div className='footer'>
     <Typography.Title level={5} style={{color:'white', textAlign:'center'}} >
-    Cryptoverse <br />
+    Cryptomaze <br />
     All Right Reserved
   </Typography.Title>
   <Space >
